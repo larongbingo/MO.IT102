0d8788dd969445907281b9003817e9ba080c2b62
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2024-12-19 22:10:47 +0800

    Week 3: Updated to match the expected in the example code

--- src/Week3AddDeleteAlgos.java
+++ src/Week3AddDeleteAlgos.java
@@ -9,16 +9,30 @@ public class Week3AddDeleteAlgos {
         var list = new UniqueNumbersLinkedList();
 
         while (true) {
-            System.out.print("Enter a number or q for quit: ");
+            System.out.print("Enter a command(quit, add, remove): ");
             var input = scanner.nextLine();
 
-            if (Objects.equals(input, "q")) {
+            if (input.equals("quit")) {
+                list.printList();
                 return;
             }
+            else if (input.equals("add")) {
+                System.out.print("Enter a number: ");
+                var num = scanner.nextInt();
+                var result = list.add(num);
 
-            var num = Integer.parseInt(input);
-
-            list.isUnique(num);
+                if (result) {
+                    System.out.println("Added the number to the list");
+                }
+                else {
+                    System.out.println("Number in the list");
+                }
+            }
+            else if (input.equals("remove")) {
+                System.out.print("Enter a number: ");
+                var num = scanner.nextInt();
+                list.remove(num);
+            }
         }
     }
 }
@@ -26,21 +40,20 @@ public class Week3AddDeleteAlgos {
 class UniqueNumbersLinkedList {
     private LinkedList<Integer> list = new LinkedList<>();
 
-    /// Returns true if the number is unique and is added into the list
-    /// False otherwise
-    public boolean isUnique(int num) {
-        var isUniqueInList = list.contains(num);
-
-        if (isUniqueInList) {
-            list.remove((Object)num);
-        }
-        else {
+    public boolean add(int num) {
+        if (!list.contains(num)) {
             list.add(num);
+            return true;
         }
 
-        System.out.println(list);
-
-        return isUniqueInList;
+        return false;
     }
 
+    public boolean remove(int num) {
+        return list.remove((Object)num);
+    }
+
+    public void printList() {
+        System.out.println(list);
+    }
 }
